390b2b6a7c4508ea8d62b9380f7ae5d6c33cc44f
Stephon Tonge <stephon.tonge75@gmail.com>
2024-03-06 16:16:33 -0600

    nav bar compatibility completed

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import logoImage from "./Smil3z_IT-Logo.png";
 
 function Logo() {
   return (
-    <div className="w-40 p-4">
+    <div className="w-40 p-4 border-2 rounded-lg border-blue-400">
       <img src={logoImage} alt="logo" />
     </div>
   );
