84fe5653450dbc10a237f6d0637d195151401694
Stephon Tonge <stephon.tonge75@gmail.com>
2024-03-20 21:40:51 -0500

    starting about me page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import VideoBackground from "./components/VideoBackground/VideoBackground.jsx";
 import Hero from "./components/Hero/Hero.jsx";
+import AboutMe from "./components/AboutMe/AboutMe";
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <VideoBackground />
       <Hero /> 
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe/AboutMe.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutMe() {
+  return <div className="text-white">About Me</div>;
+}
+
+export default AboutMe;
