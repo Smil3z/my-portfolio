065e5437f8e315d8b3aa6511f85c701ae4890b38
Stephon Tonge <stephon.tonge75@gmail.com>
2024-03-29 14:44:02 -0500

    got the tyoe animation working

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,9 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import me from "../AboutMe/me.jpeg";
+import { Typed } from "react-typed";
 
 function AboutMe() {
+  const typedRef = useRef(null);
+
+  useEffect(() => {
+    if (typedRef.current) {
+      const options = {
+        strings: [
+          "Hello everyone and welcome to my portfolio, my name is Stephon and I am a motivated Full Stack Engineer proficient in HTML, CSS, and JavaScript programming languages.",
+          "My background is in Web Development and Agile Methodologies.",
+          "I provide a high level of service to clients in custom development for applications and website proprietary customizations.",
+        ],
+        typeSpeed: 20,
+      };
+      const typed = new Typed(typedRef.current, options);
+      return () => {
+        typed.destroy();
+      };
+    }
+  }, []);
+
   return (
-    <div className="text-white items-center text-center">
-      <header className="top-500 text-center py-4">About Me</header>
+    <div className="page2 w-full h-full bg-black py-80 px-4">
+      <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
+        <img
+          src={me}
+          alt="/"
+          className="rounded-xl border-double border-sky-400 border-4"
+        />
+        <div className="text-white tracking-wide text-2xl">
+          <h1>About Me</h1>
+          <br />
+          <span ref={typedRef}></span>
+        </div>
+      </div>
     </div>
   );
 }
